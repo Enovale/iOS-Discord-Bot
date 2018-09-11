f7a2885cfb3f42cb5c94650f8f58944309ceb076
Elijah Bansley <elijahbansley@monroe.k12.wi.us>
2018-09-11 17:13:21 -0500

    Initial Commit

---
+++ SwordBot/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SwordBot
+//
+//  Created by Elijah on 9/11/18.
+//  Copyright © 2018 Elijah. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
